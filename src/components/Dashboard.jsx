c1243e9a26372c1547858f471816ba687ac29794
Dinesh Kumar <144461916+Iamdineshkumars@users.noreply.github.com>
2025-02-11 21:04:09 +0530

    Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
-import sunrise from '../assets/Icons/sunrise.png'
-import sunset from '../assets/Icons/sunset.png'
-import humidity from '../assets/Icons/humidity.png'
-import wind from '../assets/Icons/wind.png'
+
 import pressure from '../assets/Icons/pressure.png'
-import UV from '../assets/Icons/UV.png'
+
 import windy from '../assets/Icons/windy.png'
 import humiditygif from '../assets/Icons/humiditygif.gif'
 import sunrisegif from '../assets/Icons/sunrisegif.gif'
@@ -52,13 +49,13 @@ const Dashboard = ({data}) => {
          <div key={items.id} className='  lg: w-[80%] p-8 lg:h-[560px] lg:grid grid-cols-4 grid-rows-2 gap-8 max-sm:flex-col max-sm:flex max-sm:justify-center  '>{/* this div tag is second main parent for the dashbord.in which the four child divs are arranged in grid cart */}
 
 
-         <div className='border p-2 lg:w-[100%] col-span-2 rounded-2xl  max-sm: w-[120%] max-sm:h-96 shadow-xl shadow-neutral-600 opacity-80'>{/* child-1 , location */}
+         <div className='border p-2 lg:w-[100%] col-span-2 rounded-2xl  max-sm: w-[135%] max-sm:h-[100%] shadow-xl shadow-neutral-600 opacity-80'>{/* child-1 , location */}
             <div className='p-4 lg:flex max-sm:flex-col '>
                 <div className=''> {/* child-1 ,subchild-1 */}
-                   <h1 className='text-2xl font-bold font-mono border-slate-600 border-b-2'>Country:  {items.location.country} </h1>
-                   <h1 className='text-2xl font-bold font-mono mt-2 border-slate-600 border-b-2'>Region: {items.location.region}</h1>
-                   <h1 className='text-2xl font-bold font-mono mt-2 border-slate-600 border-b-2'>TimeZone: {items.location.tz_id}</h1>
-                    <h1 className='text-2xl font-mono font-bold mt-2'>Time: {items.location.localtime}</h1>
+                   <h1 className='lg:text-2xl font-bold lg:font-mono border-slate-600 border-b-2 '>Country:  {items.location.country} </h1>
+                   <h1 className='lg:text-2xl font-bold lg:font-mono mt-2 border-slate-600 border-b-2'>Region: {items.location.region}</h1>
+                   <h1 className='lg:text-2xl font-bold lg:font-mono mt-2 border-slate-600 border-b-2'>TimeZone: {items.location.tz_id}</h1>
+                    <h1 className='lg:text-2xl font-bold lg:font-mono mt-2'>Time: {items.location.localtime}</h1>
                 </div>
                 <div className=' max-sm:mt-8 ml-4'>{/* child-1 ,subchild-2 */}
                       <div><h1 className='text-4xl font-bold max-sm:text-8xl'>{items.current.temp_c}c</h1></div>
@@ -67,9 +64,9 @@ const Dashboard = ({data}) => {
               </div>
             </div>
          {/*---------------------------------------child-1-end------------------------------------------*/}
-         <div className='border p-2  col-span-2 rounded-2xl lg:w-[100%]  max-sm:w-[120%] max-sm:h-[500px] shadow-xl shadow-neutral-600 opacity-80'>{/*child-2 ,current data*/}
+         <div className='border p-2  col-span-2 rounded-2xl lg:w-[100%]  max-sm:w-[135%] max-sm:h-[500px] shadow-xl shadow-neutral-600 opacity-80'>{/*child-2 ,current data*/}
             <div className='flex flex-row items-center gap-4 max-sm:flex-col lg:mt-4 max-sm:gap-8'>
-            <div className='w-32 lg:mb-8'>{/* child-2 ,subchild-1 */}
+            <div className='w-32 lg:mt-[-30px]  '>{/* child-2 ,subchild-1 */}
                 <div><img src={items.current.condition.icon} className='h-32 '/></div>
                 <div className='pl-4'><h1 className='text-4xl font-bold font-mono'>{items.current.condition.text}</h1></div>
             </div>
@@ -89,7 +86,7 @@ const Dashboard = ({data}) => {
                </div>
                </div>
               </div>
-              <div className='p-2  h-[80%] w-[50%] grid grid-cols-2 lg:gap-4 max-sm:border-none max-sm:flex max-sm:justify-end max-sm:ml-8 max-sm:w-[70%]' >
+              <div className='p-2  h-[80%] w-[50%] grid grid-cols-2 lg:gap-4  max-sm:flex max-sm:justify-center max-sm:ml-2 max-sm:w-[100%]' >
                 <div className='max-sm:mx-4 '><img src={humiditygif} className='h-8 '/><h2 className='font-bold'>{items.current.humidity}%</h2><h3>Humidity</h3></div>
                 <div  className='max-sm:mr-4'><img src={windgif} className='h-8'/><h2 className='font-bold'>{items.current.wind_kph}</h2><h3>Wind speed</h3></div>
                 <div  className='max-sm:mx-4'><img src={pressure} className='h-8'/><h2 className='font-bold'>{items.current.pressure_in}</h2><h3>Pressure</h3></div>
@@ -100,7 +97,7 @@ const Dashboard = ({data}) => {
 
             </div>
                  {/*---------------------------------------child-2-end------------------------------------------*/} 
-                 <div className=' border p-2 w-[140%] rounded-2xl lg:h-[130%] col-span-1  max-sm:w-[120%] max-sm:h-46 shadow-xl shadow-neutral-600 opacity-80'>{/* child3, theree days forecast */}
+                 <div className=' border p-2 w-[140%] rounded-2xl lg:h-[130%] col-span-1  max-sm:w-[135%] max-sm:h-46 shadow-xl shadow-neutral-600 '>{/* child3, theree days forecast */}
              <h1 className='text-2xl flex justify-center font-bold'>3 Days Forecast</h1>
               <div className='mt-8 flex-col justify-center'>
                     
@@ -109,37 +106,40 @@ const Dashboard = ({data}) => {
                    <img src={day1[0][3]}/>
                    <h1 className='font-bold'>{day1[0][0]}</h1>/
                    <h2 className='font-bold'>{day1[0][1]}</h2>/
-                   <h2 className='font-bold'>temp: {day1[0][2]}</h2>
+                   <h2 className='font-bold'>{day1[0][2]}c</h2>
                   </div>
                   <div className='flex items-center content-end gap-2 border-slate-600 border-b-2'>
                    <img src={day1[0][3]}/>
                    <h1 className='font-bold'>{day2[0][0]}</h1>/
                    <h2 className='font-bold'>{day2[0][1]}</h2>/
-                   <h2 className='font-bold'>temp: {day2[0][2]}</h2>
+                   <h2 className='font-bold'>{day2[0][2]}c</h2>
                   </div>
                   <div className='flex items-center content-end gap-2 '>
                    <img src={day3[0][3]}/>
                    <h1 className='font-bold'>{day3[0][0]}</h1>/
                    <h2 className='font-bold'>{day3[0][1]}</h2>/
-                   <h2 className='font-bold'>temp: {day3[0][2]}</h2>
+                   <h2 className='font-bold'>{day3[0][2]}c</h2>
                   </div>
                 
                 </div>
 
             </div>
             {/*---------------------------------------child-3-end------------------------------------------*/}
-         <div className='lg:ml-[90px] lg:h-[230%] border p-2 col-span-3 w-[90%] rounded-2xl  max-sm:w-[120%] max-sm:h-[100%] shadow-xl shadow-neutral-600 opacity-80 '>{/*child-4 , houly fourcast data */}
+         <div className='lg:ml-[90px] lg:h-[230%] border p-2 col-span-3 w-[90%] rounded-2xl  max-sm:w-[135%] max-sm:h-[100%] shadow-xl shadow-neutral-600  '>{/*child-4 , houly fourcast data */}
          <h1 className='text-2xl flex justify-center font-bold '>Hourly Forecast</h1>
  
               <div className='p-4 flex flex-wrap gap-4 max-sm:flex-col '>
                 {
                   nested_data.map((stage1)=>{
-                    return <div key={stage1.id} className='lg:flex-col rounded-xl w-[100px] p-2 lg:shadow-2xl border border-slate-400 shadow-neutral-900 max-sm:border max-sm:border-slate-400 max-sm:shadow-xl max-sm:w-[100%] max-sm:flex  max-sm:items-center max-sm:p-4 shadow-gray-800'>
+                    return <div key={stage1.id} className='lg:flex-col rounded-xl w-[100px] p-2 lg:shadow-2xl border border-slate-400 shadow-neutral-900 max-sm:border max-sm:border-slate-400 max-sm:shadow-xl max-sm:w-[100%] max-sm:flex  max-sm:items-center max-sm:py-4 shadow-gray-800'>
                       <h1 className='text-2xl font-bold'>{stage1.time.split(" ")[1]}</h1>
                       <img src={stage1.condition.icon}/>
                       <h1 className='font-bold pl-2 max-sm:p-2'>{stage1.temp_c}C</h1>
+                      <div className='max-sm:block max-sm:ml-4'>
                       <img src={windy}  className='h-16'/>
-                      <h1 className='font-bold pl-2 '>{stage1.wind_kph}kph</h1>
+                      <h1 className='font-bold lg:ml-2'>{stage1.wind_kph}kph</h1>
+                      </div>
+                      
                       </div>
                   })
                 }
