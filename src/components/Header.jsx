bff8b37ca188325e6e2548d841812075551b81cf
Dinesh Kumar <144461916+Iamdineshkumars@users.noreply.github.com>
2025-02-11 21:26:00 +0530

    Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   
   return (
     <nav className='mt-8 lg:flex justify-center '> {/* headrer page*/}
-       <div ><h1 className=' text-4xl font-bold flex justify-center max-sm:text-2xl max-sm:font-bold max-sm:flex justify-center'>WETHER DASHBOARD</h1></div>
+       <div ><h1 className=' text-4xl font-bold flex justify-center max-sm:text-2xl max-sm:font-bold max-sm:flex justify-center'>WEATHER DASHBOARD</h1></div>
        <div className='mb-2 pl-8 pt-4 lg:relative left-[20%]'><ToogleButton/></div>
     </nav>
   )
